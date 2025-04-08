d700933b7bc25a70def479f12034710e2fcbe44a
djordjevicm93 <djordjevic083@gmail.com>
2025-04-08 11:32:21 +0200

    fixed loop

--- l_s.py
+++ l_s.py
@@ -1,75 +1,29 @@
-import random
+from random import *  # import all
 
-chosen_6 = list()
-# 1
-try:
-    chose_1 = int(input("Enter number"))
-    if chose_1 not in range(1, 50):
-        print("Only numbers 1 to 49")
-    elif chose_1 in chosen_6:
-        print("Same number. Enter other number.")
-    else:
-        chosen_6.append(chose_1)
-except ValueError:
-    print("Not number. Enter number.")
-# 2
-try:
-    chose_2 = int(input("Enter number"))
-    if chose_2 not in range(1, 50):
-        print("Only numbers 1 to 49")
-    elif chose_2 in chosen_6:
-        print("Same number. Enter other number.")
-    else:
-        chosen_6.append(chose_2)
-except ValueError:
-    print("Not number. Enter number.")
-# 3
-try:
-    chose_3 = int(input("Enter number"))
-    if chose_3 not in range(1, 50):
-        print("Only numbers 1 to 49")
-    elif chose_3 in chosen_6:
-        print("Same number. Enter other number.")
-    else:
-        chosen_6.append(chose_3)
-except ValueError:
-    print("Not number. Enter number.")
-# 4
-try:
-    chose_4 = int(input("Enter number"))
-    if chose_4 not in range(1, 50):
-        print("Only numbers 1 to 49")
-    elif chose_4 in chosen_6:
-        print("Same number. Enter other number.")
-    else:
-        chosen_6.append(chose_4)
-except ValueError:
-    print("Not number. Enter number.")
-# 5
-try:
-    chose_5 = int(input("Enter number"))
-    if chose_5 not in range(1, 50):
-        print("Only numbers 1 to 49")
-    elif chose_5 in chosen_6:
-        print("Same number. Enter other number.")
-    else:
-        chosen_6.append(chose_5)
-except ValueError:
-    print("Not number. Enter number.")
-# 6
-try:
-    chose_6 = int(input("Enter number"))
-    if chose_6 not in range(1, 50):
-        print("Only numbers 1 to 49")
-    elif chose_6 in chosen_6:
-        print("Same number. Enter other number.")
-    else:
-        chosen_6.append(chose_6)
-except ValueError:
-    print("Not number. Enter number.")
+chosen_6 = list()  # empty list for 6 entered numbers
+draw_6 = set()  # empty set for 6 draw numbers
 
-sorted_chosen = sorted(chosen_6)
-draw_random_6 = sorted(random.sample(range(1, 50), 6))
+def chose_6():  # function for enter and filtrate 6 numbers
+    while len(chosen_6) < 6:
+        enter_6 = input("Enter number: ")
+        try:  # filtrate <, >, str, same
+            if enter_6 not in range(1, 50):  # <, >
+                print("Only numbers 1 to 49")
+            elif enter_6 in chosen_6:  # same
+                print("Same number. Enter other number.")
+            else:
+                chosen_6.append(enter_6)  # if number pass if elif, go to list
+        except ValueError:  # str
+            print("Not number.")
+    return chosen_6.sort()
 
-print("Chosen numbers", sorted_chosen)
-print("Draw numbers", draw_random_6)
+# draw lotto numbers
+while len(draw_6) < 6:
+    draw_6.add(randint(1, 50))
+
+chose_6()
+print("Your numbers: ", chosen_6)
+print("Lotto numbers: ", draw_6)
+
+# missing count numbers
+
