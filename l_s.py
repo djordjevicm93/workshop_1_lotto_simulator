71cf8de91efea985f189338bff7189ce7d34c3ca
djordjevicm93 <djordjevic083@gmail.com>
2025-04-15 13:19:47 +0200

    count fixed

--- l_s.py
+++ l_s.py
@@ -1,7 +1,4 @@
-from random import randint
-
-chosen_6 = list()  # empty list for 6 entered numbers
-draw_6 = set()  # empty set for 6 draw numbers
+from random import shuffle
 
 def chose_6():
     """
@@ -10,27 +7,43 @@ def chose_6():
     Enter 6 number from 1 to 49. Computer will draw 6 numbers.
     You will se if you guessed 3, 4, 5, 6 or nothing.
     """
+    chosen_6 = list()  # empty list for 6 entered numbers
     while len(chosen_6) < 6:
-        enter_6 = input("Enter number: ")
+        enter_6 = input("Enter number 1 - 49: ")
         try:
-            if int(enter_6) < 1 or int(enter_6) > 49:
+            number = int(enter_6)
+            if number < 1 or number > 49:
                 print("Only numbers 1 to 49")
-            elif enter_6 in chosen_6:
+            elif number in chosen_6:
                 print("Same number. Enter other number.")
             else:
-                chosen_6.append(enter_6)  # if number pass if elif, go to list
+                chosen_6.append(number)  # if number pass if elif, go to list
         except ValueError:  # str
-            print("Not number.")
+            print("That is not number.")
     return chosen_6
 
 # draw lotto numbers
-while len(draw_6) < 6:
-    draw_6.add(randint(1, 50))
+draw_6 = list(range(1, 50))
+shuffle(draw_6)
+drawn_numbers = draw_6[:6]
 
-chose_6()
-print("Your numbers: ", sorted(list(chosen_6)))
-print("Lotto numbers: ", sorted(list(draw_6)))
+#chose_6()
+chosen_6 = chose_6()
+sorted_6 = sorted(chosen_6)
+print("Your numbers: ", sorted_6)
+print("Lotto numbers: ", sorted(drawn_numbers))
 
-# count numbers
-count_numbers = set(chosen_6) & set(draw_6)
-print("Win:", len(count_numbers))
+# count matched
+matches = [x for x in sorted_6 if x in drawn_numbers]
+match_count = len(matches)
+print("Matched numbers:", matches)
+print("Total matches:", match_count)
+
+rewards = {
+    6: "JACKPOT! You hit all 6 numbers!",
+    5: "Great job! You matched 5 numbers!",
+    4: "Nice! You matched 4 numbers!",
+    3: "You matched 3 numbers. Small win!",
+}
+
+print("Result:", rewards.get(match_count))
